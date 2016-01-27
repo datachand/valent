dd2e7dca6d8269b48aeb44779e921bb553dd84b2
Chandresh Rajkumar Manonmani <data.chand@gmail.com>
2016-01-27 16:20:42 +0530

    FeedBack

---
+++ README.md
@@ -0,0 +1,8 @@
+- "How do like this view"
+    - rating star
+- "What can be better?"
+    - 140 char 
+- "What would you like to see?"
+    - 140 char
+- customized feedback with page
+
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-$null 
+(function () {
+
+    "use strict";
+
+    angular.module('valent', ['ngResource', 'ngSanitize', 'ngAnimate', 'ui.router', 'ui.bootstrap']);
+
+
+    function HelpCtrl ($scope, $log, $http) {
+
+    }
+
+    HelpCtrl.$inject = ["$scope", "$log", "$http"];
+
+    angular.module('valent').controller('HelpCtrl', HelpCtrl);
+
+})();
